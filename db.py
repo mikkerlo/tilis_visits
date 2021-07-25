21e0af973fc56c8499e5ba252fbbb378560b86b6
Mikhail Semenov <mikhail.semenov.al@gmail.com>
2021-07-26 00:13:54 +0200

    Add black as formating tool

--- db.py
+++ db.py
@@ -1,36 +1,31 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy import DateTime, Table
-from sqlalchemy.orm import relationship, backref
-
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.sql.sqltypes import Float
 
 Base = declarative_base()
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine("sqlite:///:memory:", echo=True)
 
 
 class VisitVisiter(Base):
-    __tablename__ = 'vist_visiter'
-    visit_id = Column(Integer, ForeignKey('visit.visit_id'), primary_key=True)
-    visiter_id = Column(Integer, ForeignKey('visiter.visiter_id'), primary_key=True)
+    __tablename__ = "vist_visiter"
+    visit_id = Column(Integer, ForeignKey("visit.visit_id"), primary_key=True)
+    visiter_id = Column(Integer, ForeignKey("visiter.visiter_id"), primary_key=True)
     paid = Column(Boolean)
     visiter = relationship("Visiter")
     visit = relationship("Visit")
 
 
 class Visiter(Base):
-    __tablename__ = 'visiter'
+    __tablename__ = "visiter"
     visiter_id = Column(Integer, primary_key=True)
     name = Column(String)
     tg_id = Column(Integer)
 
 
 class Visit(Base):
-    __tablename__ = 'visit'
+    __tablename__ = "visit"
     visit_id = Column(Integer, primary_key=True)
     name = Column(String)
     date = Column(DateTime)
     total_payment = Column(Float)
-
